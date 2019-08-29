97a4e4d388becfb4bb52b1bb790abfc478718f01
Joey Segura <10735119@my.uvu.edu>
2019-08-29 15:40:16 -0600

    Added Print Script

---
+++ Project1/Assets/Scripts/startScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        print("Who are you?");
+        print("Where is the bathroom?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
