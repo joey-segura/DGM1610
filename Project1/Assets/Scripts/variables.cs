5a762517c636b09cf5d0dbaab26e38491b740b7c
Joey Segura <10735119@my.uvu.edu>
2019-09-03 15:21:46 -0600

    Added Collision Script

---
+++ Project1/Assets/Scripts/variables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class variables : MonoBehaviour
+{
+	public float floatValue = 5f;
+	public int intValue;
+	public string stringValue = "Bob";
+	public int firePower;
+	public UnityEvent Event;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		Event.Invoke();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
