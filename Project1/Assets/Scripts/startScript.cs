1d1d23c1d23a4d5c60ab2db83f345afae986978c
Joey Segura <10735119@my.uvu.edu>
2019-09-04 14:21:58 -0600

    Added Destroy Script

--- Project1/Assets/Scripts/startScript.cs
+++ Project1/Assets/Scripts/startScript.cs
@@ -7,9 +7,7 @@ public class startScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World");
-        print("Who are you?");
-        print("Where is the bathroom?");
+        print("Game Start: Success");
     }
 
     // Update is called once per frame
--- Project1/Assets/Scripts/variables.cs
+++ Project1/Assets/Scripts/variables.cs
@@ -8,13 +8,17 @@ public class variables : MonoBehaviour
 {
 	public float floatValue = 5f;
 	public int intValue;
-	public string stringValue = "Bob";
+	public string stringValue = "Pixeltic";
 	public int firePower;
 	public UnityEvent Event;
 	
 	private void OnTriggerEnter(Collider other)
 	{
-		Event.Invoke();
+		if (other.gameObject.name == "Bomb")
+		{
+			print("GAME OVER");
+			Destroy(this.gameObject);
+		}
 	}
 
 	// Use this for initialization
