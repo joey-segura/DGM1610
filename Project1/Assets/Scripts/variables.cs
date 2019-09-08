17ea7110317304d4efa6f7d3ce38fc6b9d184c0f
Joey Segura <10735119@my.uvu.edu>
2019-09-08 13:35:52 -0600

    Fixed Cube Movement

--- Project1/Assets/Scripts/variables.cs
+++ Project1/Assets/Scripts/variables.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class Variables : MonoBehaviour
+public class variables : MonoBehaviour
 {
 	public float floatValue = 5f;
 	public int intValue;
 	public string stringValue = "Pixeltic";
 	public int firePower;
 	public UnityEvent Event;
-	
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.name == "Bomb")
 		{
 			print("GAME OVER");
 			Destroy(this.gameObject);
+			Destroy(GameObject.Find("Sparks"));
 		}
 	}
 }
