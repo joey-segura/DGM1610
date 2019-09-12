8fc8085acf4897350553262a0fd82b721b464f7e
Joey Segura <10735119@my.uvu.edu>
2019-09-12 15:33:18 -0600

    DogScript+EmitScript

---
+++ Project1/Assets/Scripts/killObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class killObject : MonoBehaviour
+{
+    public UnityEvent die;
+
+    private void OnMouseDown()
+    {
+        die.Invoke();
+    }
+}
---
+++ Project1/Assets/Scripts/particleControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleControl : MonoBehaviour
+{
+    public ParticleSystem particles;
+    void Start()
+    {
+        particles.Emit(count: 100);
+    }
+}
