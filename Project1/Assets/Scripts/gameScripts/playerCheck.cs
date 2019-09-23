94ecf768445759423127e5fc7c4370be116eb265
Joey Segura <10735119@my.uvu.edu>
2019-09-23 14:01:17 -0600

    Added playerCheck

---
+++ Project1/Assets/Scripts/gameScripts/playerCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCheck : MonoBehaviour
+{
+    private void Update()
+    {
+        if (GameObject.Find("player") != null)
+        {
+            Debug.Log("I am here.");
+        }
+        else
+        {
+            Debug.Log("I am not here.");
+        }
+    }
+}
---
+++ Project1/Assets/Scripts/gameScripts/spikeDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeDeath : MonoBehaviour
+{ 
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.name == "spikes")
+        {
+            print("YOU DIED");
+            Destroy(this.gameObject);
+        }
+    }
+}
