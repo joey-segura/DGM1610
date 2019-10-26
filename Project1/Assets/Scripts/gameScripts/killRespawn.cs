e62094b9057df248aade4fc2301c5da7b3834c31
Joey Segura <10735119@my.uvu.edu>
2019-10-26 13:26:15 -0600

    KillRespawnScript(NOT WORKING)

---
+++ Project1/Assets/Scripts/gameScripts/killRespawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killRespawn : MonoBehaviour
+{
+    [SerializeField] private Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "respawnPoint")
+        {
+            this.gameObject.transform.position = respawnPoint.position;
+        }
+    }
+}    
