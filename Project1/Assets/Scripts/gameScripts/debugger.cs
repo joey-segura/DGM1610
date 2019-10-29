aa3194fb81028703b0eaec22910247d808cf4d3f
Joey Segura <10735119@my.uvu.edu>
2019-10-29 15:00:03 -0600

    enum

---
+++ Project1/Assets/Scripts/gameScripts/debugger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class debugger : ScriptableObject
+{
+    public void onDebug(string words)
+    {
+        Debug.Log(words);
+    }
+}
---
+++ Project1/Assets/Scripts/gameScripts/enumSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class enumSwitch : MonoBehaviour
+{
+    public enum States
+    {
+        Start,
+        Playing,
+        End
+    }
+
+    public States currentState;
+
+    public UnityEvent onStartEvent, onPlayingEvent, onEndEvent;
+    void Update()
+    {
+        switch (currentState)
+        {
+            case States.Start:
+                onStartEvent.Invoke();
+                break;
+            case States.Playing:
+                onPlayingEvent.Invoke();
+                break;
+            case States.End:
+                onEndEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
