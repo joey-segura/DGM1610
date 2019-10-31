6de7dd49c9875c1b8c83a88b1d78d331b0fe2e7f
Joey Segura <10735119@my.uvu.edu>
2019-10-31 15:04:56 -0600

    AI Test

---
+++ Project1/Assets/agentHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agentHandler : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Transform destinationobj;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = destinationobj.position;
+    }
+}
