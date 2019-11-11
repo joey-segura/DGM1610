4113d215c9bb82a638da7e141ef2482e55a048f5
Joey Segura <10735119@my.uvu.edu>
2019-11-10 17:44:13 -0700

    NewLVLNewOBSTCL

--- Project1/Assets/Scripts/gameScripts/followUpdate.cs
+++ Project1/Assets/Scripts/gameScripts/followUpdate.cs
@@ -14,3 +14,4 @@ public class followUpdate : MonoBehaviour
         }
     }
 }
+
---
+++ Project1/Assets/Scripts/gameScripts/gcCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gcCollide : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
