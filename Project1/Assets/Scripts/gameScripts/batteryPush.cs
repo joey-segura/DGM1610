dbaff3e82f04efbd25636d2f1110c354805285dd
Joey Segura <10735119@my.uvu.edu>
2019-11-11 09:01:10 -0700

    PushObjects Script

---
+++ Project1/Assets/Scripts/gameScripts/batteryPush.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batteryPush : MonoBehaviour
+{
+    public float push = 5.0f;
+    private movement3 speedAccess;
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rb = hit.collider.attachedRigidbody;
+
+        if (rb == null || rb.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+        push = speedAccess.moveSpeed;
+
+        Vector3 direction = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        rb.velocity = direction * push;
+    }
+    private void Start()
+    {
+        speedAccess = gameObject.GetComponent<movement3>();
+    }
+}
