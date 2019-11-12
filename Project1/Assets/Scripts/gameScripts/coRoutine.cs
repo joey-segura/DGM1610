e8f81ec7256ee3eed60b1ec220fcde732d9282b6
Joey Segura <10735119@my.uvu.edu>
2019-11-12 15:49:42 -0700

    coRoutine

---
+++ Project1/Assets/Scripts/gameScripts/coRoutine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public abstract class coRoutine : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float seconds = 10f;
+    private int i = 10;
+    public int counter;
+    public bool canRun;
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        CallCoRoutine();
+    }
+    
+    public void CallCoRoutine()
+    {
+        StartCoroutine(RunCoRoutine());
+    }
+    
+    IEnumerator RunCoRoutine()
+    {
+        startEvent.Invoke();
+        
+        while (i > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(1f);
+            counter--;
+        }
+        while (canRun == true)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(1f);
+            counter--;
+        }
+        endEvent.Invoke();
+    }
+}
