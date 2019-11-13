32e11b0077164bafbf00e89fb392ad93b771ff7e
Joey Segura <10735119@my.uvu.edu>
2019-11-13 14:01:12 -0700

    BatKill
    Bat now dies on collision with crystals!

--- Project1/Assets/Scripts/gameScripts/gcCollide.cs
+++ Project1/Assets/Scripts/gameScripts/gcCollide.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,11 @@ public class gcCollide : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Obstacle"))
+        if (other.gameObject.name == "GC")
+        {
+            this.gameObject.SetActive(false);
+        }
+        if (other.gameObject.name == "GC2")
         {
             this.gameObject.SetActive(false);
         }
