05f78bb46ec7f32867ed5ad4851899ea4bd94255
Joey Segura <10735119@my.uvu.edu>
2019-11-14 15:44:44 -0700

    Fixed Bat Rotation

---
+++ Project1/Assets/Scripts/gameScripts/coRoutineClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coRoutineClass : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            Debug.Log("Working");
+            CallCoRoutine();
+        }
+    }
+    public void CallCoRoutine()
+    {
+        StartCoroutine(OnRun());
+    }
+    IEnumerator OnRun()
+    {
+        yield return new WaitForSeconds(3f);
+            Debug.Log("Hello.");
+    }
+}
--- Project1/Assets/Scripts/gameScripts/followUpdate.cs
+++ Project1/Assets/Scripts/gameScripts/followUpdate.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class followUpdate : MonoBehaviour
 {
     public GameObject target;
+    public Transform followPoint;
     void Update()
     {
+        transform.LookAt(followPoint);
         target = GameObject.Find("player(Clone)");
+        followPoint = GameObject.Find("player(Clone)").transform;
         if (target != null)
         {
             this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, target.transform.position, 0.1f);
