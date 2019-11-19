f8c470e186c289654165053d2ffa0ddceaec5e56
Joey Segura <10735119@my.uvu.edu>
2019-11-19 15:20:39 -0700

    Follow Re-Work

--- Project1/Assets/Scripts/gameScripts/followUpdate.cs
+++ Project1/Assets/Scripts/gameScripts/followUpdate.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,29 @@ public class followUpdate : MonoBehaviour
 {
     public GameObject target;
     public Transform followPoint;
+    public int triggerRange;
+
+    private void Start()
+    {
+        StartCoroutine(GetPlayer());
+    }
+
+    public IEnumerator GetPlayer()
+    {
+        yield return null;
+        target = GameObject.Find("player(Clone)");
+        followPoint = target.transform;
+    }
     void Update()
     {
         transform.LookAt(followPoint);
-        target = GameObject.Find("player(Clone)");
-        followPoint = GameObject.Find("player(Clone)").transform;
         if (target != null)
         {
-            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, target.transform.position, 0.1f);
+            if((Vector3.Distance(transform.position,target.transform.position)<triggerRange))
+            {
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position,
+                    target.transform.position, 0.1f);
+            }
         }
     }
 }
