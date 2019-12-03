53091f431b5308cbcf6f961a85eec09bce0a6aec
Joey Segura <10735119@my.uvu.edu>
2019-12-02 20:52:19 -0700

    fallingPlatform(notWorking)

---
+++ Project1/Assets/Scripts/gameScripts/fallFloor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallFloor : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(FallDelay()); 
+    }
+    private IEnumerator FallDelay()
+    {
+        yield return new WaitForSeconds(2);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
