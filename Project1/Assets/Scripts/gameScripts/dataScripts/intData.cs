9e61b405c70f3266ee9e195d940468fc5cf6f314
Joey Segura <10735119@my.uvu.edu>
2019-12-05 11:55:16 -0700

    LevelWork

--- Project1/Assets/Scripts/gameScripts/dataScripts/intData.cs
+++ Project1/Assets/Scripts/gameScripts/dataScripts/intData.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
 public class intData : ScriptableObject
 {
     public int value = 1;
-
     public void UpdateValue(int number)
     {
         value += number;
