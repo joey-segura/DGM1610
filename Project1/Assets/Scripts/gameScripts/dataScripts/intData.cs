8eaf0a247a9fe55ab13a62445e2dd537326609de
Joey Segura <10735119@my.uvu.edu>
2019-12-05 15:38:23 -0700

    displayText (unfinishedScript)

--- Project1/Assets/Scripts/gameScripts/dataScripts/intData.cs
+++ Project1/Assets/Scripts/gameScripts/dataScripts/intData.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 [CreateAssetMenu]
 public class intData : ScriptableObject
 {
-    public int value = 1;
+    public int value = 1, minValue = 0, maxValue = 10;
     public void UpdateValue(int number)
     {
         value += number;
+        if (value > maxValue)
+        {
+            value = maxValue;
+        }
+        if (value < minValue)
+        {
+            value = minValue;
+        }
     }
 }
---
+++ Project1/Assets/Scripts/gameScripts/displayText.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayText : MonoBehaviour
+{
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
