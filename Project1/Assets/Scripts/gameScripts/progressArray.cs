8f25d8ff7f331e919f4debdbf436c0a60b19b6b4
Joey Segura <10735119@my.uvu.edu>
2019-12-08 14:36:50 -0700

    FinalBuild

--- Project1/Assets/Scripts/gameScripts/progressArray.cs
+++ Project1/Assets/Scripts/gameScripts/progressArray.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class progressArray : MonoBehaviour
 {
     [SerializeField] private GameObject[] coins;
-    void Start()
+    public Text changingText;
+    private void OnTriggerEnter(Collider other)
+    {
+        TextUpdate();
+    }
+    public void TextUpdate()
     {
         for (int i = 0; i < coins.Length; i++)
         {
-            Debug.Log(coins[i].name + " still not found!");
+            changingText.text = (coins[i].name + " still not found!");
         }
+        changingText.text = "Congratulations! You collected all of the coins!";
     }
 }
